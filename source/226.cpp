85cdca32156659724f3020e855cbb6596c2f8953
wangtss <wangtstim@gmail.com>
2018-01-10 20:44:12 +0800

    easy

---
+++ source/226.cpp
@@ -0,0 +1,8 @@
+TreeNode* invertTree(TreeNode* root) {
+	if (!root) return root;
+	TreeNode *left = invertTree(root->left);
+	TreeNode * right = invertTree(root->right);
+	root->left = right;
+	root->right = left;
+	return root;
+}
