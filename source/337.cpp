a897a87508eb0b07c87ea514929cc5520697ad8f
wangtss <wangtstim@gmail.com>
2018-01-25 15:04:09 +0800

    medium

--- source/337.cpp
+++ source/337.cpp
@@ -12,3 +12,14 @@
 		hash[node] = val;
 		return val;
 	}
+	int rob(TreeNode* root) {
+		vector<int> res = dfs(root);
+		return max(res[0], res[1]);
+	}
+	vector<int> dfs(TreeNode *node) {
+		if (!node) return { 0, 0 };
+		vector<int> left = dfs(node->left), right = dfs(node->right), res(2, 0);
+		res[0] = max(left[0], left[1]) + max(right[0], right[1]);
+		res[1] = left[0] + right[0] + node->val;
+		return res;
+	}
